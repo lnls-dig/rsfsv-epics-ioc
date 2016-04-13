17265a39d88b0fea46f810a47bf3bcaa315dda36
Vfinotti <finotao@gmail.com>
2016-04-13 17:07:45 -0300

    Included initial files

---
+++ iocBoot/iocrsfsv/st.cmd
@@ -0,0 +1,30 @@
+#!../../bin/linux-x86_64/rsfsv
+
+## You may have to change rsfsv to something else
+## everywhere it appears in this file
+
+< envPaths
+
+#cd ${TOP}
+
+# Load device configurations
+< device.config
+
+epicsEnvSet("TOP", "../..")
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/rsfsvApp/Db")
+
+## Register all support components
+dbLoadDatabase("$(TOP)/dbd/rsfsv.dbd",0,0)
+rsfsv_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadRecords("${TOP}/rsfsvApp/Db/rsfsv.db", "P=DIG, DEVICE_INST = $(DEVICE_INST), PORT=rsfsv_port, ADDR=0, TIMEOUT=1")
+
+# Create the asyn port to use the VXI11 protocol
+vxi11Configure("rsfsv_port","$(DEVICE_IP)",1,1000,"inst")
+
+#cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=finottiHost"
