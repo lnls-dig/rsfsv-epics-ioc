2b62411417440d26f5115a15ac9093f82de436f8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-07-10 11:39:28 +0000

    Allow using environment variable paramaters

--- iocBoot/iocrsfsv/run.sh
+++ iocBoot/iocrsfsv/run.sh
@@ -3,9 +3,10 @@
 # Tests if variable EPICS_HOST_ARCH exists. If not, how message
 : ${EPICS_HOST_ARCH:?"Environment variable needs to be set"}
 
-
 # Select device IP.
-DEVICE_IP="$1"
+if [ -z "$DEVICE_IP" ]; then
+    DEVICE_IP="$1"
+fi
 
 if [ -z "$DEVICE_IP" ]; then
     echo "\"DEVICE_IP\" variable unset."
@@ -13,7 +14,9 @@ if [ -z "$DEVICE_IP" ]; then
 fi
 
 # Select instance of the device.
-DEVICE_INST="$2"
+if [ -z "$DEVICE_INST" ]; then
+    DEVICE_INST="$2"
+fi
 
 if [ -z "$DEVICE_INST" ]; then
     echo "\"DEVICE_INST\" variable unset."
