6e45b2520806b8b2c351b091e217177c8a07207a
andrei_pereira <andrei.pereira@lnls.br>
2018-01-19 10:41:59 -0200

    Check if directories exist before trying to copy them

--- install/install.sh
+++ install/install.sh
@@ -26,7 +26,9 @@ mkdir -p "$INSTALL_DIR"
 
 # Copy generated EPICS files
 for dir in bin lib db dbd; do
-    cp -r "$TOP_DIR/$dir" "$INSTALL_DIR"
+    if [ -d "$dir" ]; then
+        cp -r "$TOP_DIR/$dir" "$INSTALL_DIR"
+    fi
 done
 
 ### *App files ###
