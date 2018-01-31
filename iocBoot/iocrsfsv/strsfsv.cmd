fca1c958106a5173fe661923f792f5773b0b5b90
andrei_pereira <andrei.pereira@lnls.br>
2018-01-31 10:50:20 -0200

    iocBoot/iocrsfsv/: Fix save_restore.cmd and change .sav files names
    iocBoot/iocrsfsv/save_restore.cmd: Fix save restore status prefix.
    iocBoot/iocrsfsv/strsfsv.cmd: Set save files names instead of using default.

--- iocBoot/iocrsfsv/strsfsv.cmd
+++ iocBoot/iocrsfsv/strsfsv.cmd
@@ -24,3 +24,4 @@ iocInit
 
 # Create manual trigger for Autosave
 create_triggered_set("auto_settings_rsfsv.req", "${P}${R}SaveTrg", "P=${P}, R=${R}")
+set_savefile_name("auto_settings_rsfsv.req", "auto_settings_${P}${R}.sav")
